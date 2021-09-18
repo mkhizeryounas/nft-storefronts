08044ef3332ede628cd4f3d39280f88a451386db
Khizer Younas <m.khizeryounas@gmail.com>
2021-09-18 08:46:17 +0500

    feat: added fe implementation

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,136 @@
-import React, { Component } from 'react';
-import logo from '../logo.png';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Web3 from 'web3';
+import Color from '../abis/Color.json';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+const App = () => {
+  const [account, setAccount] = useState('');
+  const [contract, setContract] = useState(null);
+  const [totalSupply, setTotalSupply] = useState(0);
+  const [colors, setColors] = useState([]);
+  const [inputColor, setInputColor] = useState('');
+
+  useEffect(() => {
+    loadWeb3();
+    loadBlockchainData();
+  }, []);
+
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        'Non-Ethereum browser detected. You should consider trying MetaMask!'
+      );
+    }
+  };
+
+  const loadBlockchainData = async () => {
+    const web3 = window.web3;
+    // Load account
+    const accounts = await web3.eth.getAccounts();
+    setAccount(accounts[0]);
+
+    const networkId = await web3.eth.net.getId();
+    const networkData = Color.networks[networkId];
+    if (networkData) {
+      const abi = Color.abi;
+      const address = networkData.address;
+      const loadedContract = new web3.eth.Contract(abi, address);
+      setContract(loadedContract);
+      const contractTotalSupply = await loadedContract.methods
+        .totalSupply()
+        .call();
+      setTotalSupply(contractTotalSupply);
+      const colorList = [...colors];
+      for (var i = 1; i <= contractTotalSupply; i++) {
+        const color = await loadedContract.methods.colors(i - 1).call();
+        colorList.push(color);
+      }
+      setColors(colorList);
+    } else {
+      window.alert('Smart contract not deployed to detected network.');
+    }
+  };
+
+  const mint = (color) => {
+    color = color.startsWith('#') ? color : `#${color}`;
+    console.log('color', color);
+    contract.methods
+      .mint(color)
+      .send({ from: account })
+      .once('receipt', (receipt) => {
+        console.log('receipt', receipt);
+        setColors([...colors, color]);
+      });
+  };
+
+  return (
+    <div>
+      <nav className='navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow'>
+        <a
+          className='navbar-brand col-sm-3 col-md-2 mr-0'
+          href='http://www.dappuniversity.com/bootcamp'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Color Tokens
+        </a>
+        <ul className='navbar-nav px-3'>
+          <li className='nav-item text-nowrap d-none d-sm-none d-sm-block'>
+            <small className='text-white'>
+              <span id='account'>
+                {totalSupply} - {account}
+              </span>
+            </small>
+          </li>
+        </ul>
+      </nav>
+      <div className='container-fluid mt-5'>
+        <div className='row'>
+          <main role='main' className='col-lg-12 d-flex text-center'>
+            <div className='content mr-auto ml-auto'>
+              <h1>Issue Token</h1>
+              <form
+                onSubmit={(event) => {
+                  event.preventDefault();
+                  mint(inputColor);
+                }}
+              >
+                <input
+                  type='text'
+                  className='form-control mb-1'
+                  placeholder='e.g. #FFFFFF'
+                  onChange={(input) => {
+                    setInputColor(input.target.value);
+                  }}
+                />
+                <input
+                  type='submit'
+                  className='btn btn-block btn-primary'
+                  value='MINT'
+                />
+              </form>
+            </div>
+          </main>
+        </div>
+        <hr />
+        <div className='row text-center'>
+          {colors.map((color, key) => {
+            return (
+              <div key={key} className='col-md-3 mb-3'>
+                <div className='token' style={{ backgroundColor: color }}></div>
+                <div>{color}</div>
               </div>
-            </main>
-          </div>
+            );
+          })}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
